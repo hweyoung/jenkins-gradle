7f81cc504e1ff83efb45d5e06f597648182fc4a1
음휘영 <76817418+hweyoung@users.noreply.github.com>
2023-06-16 22:02:45 +0900

    Update HelloController.java

--- src/main/java/com/example/glCloud/HelloController.java
+++ src/main/java/com/example/glCloud/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello(){
-        return "Hello World!! ";
+        return "Hello World!tests! ";
     }
 }
